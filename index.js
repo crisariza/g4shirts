f33fe15426c6187860bf302e9447d1daab92f332
Cristian Ariza <cristianariza89@gmail.com>
2021-04-09 03:45:00 -0300

    consolelog

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const { API_PORT } = process.env;
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT || API_PORT, async () => {
-    console.log(`The server is listening at the port ${API_PORT}`);
+    console.log(
+      `The server is listening at the port ${process.env.PORT || API_PORT}`
+    );
     bulkScript();
   });
 });
