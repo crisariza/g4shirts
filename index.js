96bd9dcdfdd2d535edb54c8923ce8f2af4f7b967
Cristian Ariza <cristianariza89@gmail.com>
2021-04-30 11:35:54 -0300

    fix

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { bulkScript } = require("./bulkscript.js");
-const { API_PORT } = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT || API_PORT, async () => {
     console.log(
-      `The server is listening at the port ${process.env.PORT || API_PORT}`
+      `The server is listening at the port ${process.env.PORT || 5000}`
     );
     bulkScript();
   });
